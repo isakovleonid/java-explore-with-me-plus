1efffdde2ab0e9ef5817dfec49e28d528856cc72
Isakov Leonid <isakov.leonid@gmail.com>
2025-06-27 19:40:14 +0300

    Изменен пакет у main-класса модуля stats-server

--- stats/stats-server/src/main/java/ru/practicum/server/StatsServer.java
+++ stats/stats-server/src/main/java/ru/practicum/server/StatsServer.java
@@ -1,11 +1,9 @@
-package ru.practicum;
+package ru.practicum.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ru.practicum.server"})
 public class StatsServer {
     public static void main(String[] args) {
         SpringApplication.run(StatsServer.class, args);
