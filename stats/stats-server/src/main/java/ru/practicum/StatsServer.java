610d73ba34217f258661385348e73ab48a01c2db
Isakov Leonid <isakov.leonid@gmail.com>
2025-06-27 19:31:36 +0300

    1) Исключаем .idea из git 2) В StatsServer Добавлено сканирование пакет ru.practicum.server

--- stats/stats-server/src/main/java/ru/practicum/StatsServer.java
+++ stats/stats-server/src/main/java/ru/practicum/StatsServer.java
@@ -2,8 +2,10 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"ru.practicum.server"})
 public class StatsServer {
     public static void main(String[] args) {
         SpringApplication.run(StatsServer.class, args);
