f0306f97ff7c383f796b51006c303e1cb229898b
Авралев Максим <avralevm@yandex.ru>
2025-07-01 22:59:28 +0500

    Убрал лишний import

--- main-service/src/main/java/ru/practicum/users/dto/in/NewUserRequest.java
+++ main-service/src/main/java/ru/practicum/users/dto/in/NewUserRequest.java
@@ -2,7 +2,6 @@ package ru.practicum.users.dto.in;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
