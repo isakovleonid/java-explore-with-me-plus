32ae03db56cf52aa5bcd057f34d1d31fec6285e3
Авралев Максим <avralevm@yandex.ru>
2025-07-18 22:59:05 +0500

    Корректировка для прохождения тестов

--- main-service/src/main/java/ru/practicum/comments/service/CommentServiceImpl.java
+++ main-service/src/main/java/ru/practicum/comments/service/CommentServiceImpl.java
@@ -1,7 +1,6 @@
 
 package ru.practicum.comments.service;
 
-import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
